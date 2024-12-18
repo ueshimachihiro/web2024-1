2e1ae8961f3c644a63936ea651906b1075a22da9
ueshimachihiro <167832794+ueshimachihiro@users.noreply.github.com>
2024-12-18 22:37:27 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,9 @@
+function uruudosi(){
+ var x = Number(document.getElementById('input7').value);
+ if(y/(x*x) < 18.5)
+  {alert('低体重');}
+  else if(y/(x*x) < 25)
+  {alert('普通');}
+  else{alert('肥満');}
+}
 
